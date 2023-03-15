41e4987504a2c82a3b7c7ee5debdbad25a281c02
WeirdEraCont <32410147+WeirdEraCont@users.noreply.github.com>
2023-03-15 18:56:37 +0100

    Make branch
    Save changes somewhere

--- SeriousFalafels/Assets/Scripts/ColorChange.cs
+++ SeriousFalafels/Assets/Scripts/ColorChange.cs
@@ -9,11 +9,13 @@ public class ColorChange : MonoBehaviour
     private PlayerTurnController controller;
     [SerializeField]
     private GameObject canva;
+    private RadioButtonSystem radioButton;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GameObject.FindWithTag("GameController").GetComponent<PlayerTurnController>();
+        radioButton = canva.transform.Find("Panel").gameObject.transform.Find("OptionsButtons").GetComponent<RadioButtonSystem>();
     }
 
     // Update is called once per frame
@@ -25,6 +27,7 @@ public class ColorChange : MonoBehaviour
     private void OnMouseDown() {
         if (currentMode == 1 && !canva.activeSelf) {
             canva.SetActive(true);
+            radioButton.SetTarget(gameObject);
         }
     }
 }
--- SeriousFalafels/Assets/Scripts/RadioButtonSystem.cs
+++ SeriousFalafels/Assets/Scripts/RadioButtonSystem.cs
@@ -5,6 +5,7 @@ using System.Linq;
 public class RadioButtonSystem : MonoBehaviour
 {
     ToggleGroup toggleGroup;
+    private GameObject target;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +16,12 @@ public class RadioButtonSystem : MonoBehaviour
     {
         Toggle toggle = toggleGroup.ActiveToggles().FirstOrDefault();
         Debug.Log(toggle.name + " _ " + toggle.GetComponentInChildren<Text>().text);
+        transform.parent.gameObject.transform.parent.gameObject.SetActive(false); // get canva parent
+        SpriteRenderer targetSpriteRend = target.GetComponent<SpriteRenderer>();
+        Sprite targetCurrentSprite = targetSpriteRend.sprite;
+    }
+
+    public void SetTarget(GameObject obj) {
+        target = obj;
     }
 }
